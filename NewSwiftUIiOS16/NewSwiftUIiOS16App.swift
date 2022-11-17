76dc5df860a91d60688305e8c00457f764734076
Oleksii Leshchenko <84497187+alexbreamdev@users.noreply.github.com>
2022-11-17 12:49:04 +0200

    Initial Commit

---
+++ NewSwiftUIiOS16/NewSwiftUIiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  NewSwiftUIiOS16App.swift
+//  NewSwiftUIiOS16
+//
+//  Created by Oleksii Leshchenko on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewSwiftUIiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
